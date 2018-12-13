17df261fd97691b77f4aaad03f4bb4063cdfa686
xingcbrew <xingcbrew@gmail.com>
2018-12-13 11:09:46 -0500

    mapped gambia files

--- read_in_function.R
+++ read_in_function.R
@@ -5,6 +5,8 @@ library(dplyr)
 library(reshape2)
 library(readr)
 
+##### MALI #######
+
 setwd('/Users/xing/Documents/vida/mali_data')
 
 # read in 
@@ -33,3 +35,53 @@ write.csv(va1_map, 'va1_map.csv')
 write.csv(va2_map, 'va2_map.csv')
 write.csv(cm_map, 'child_mali_map.csv')
 write.csv(nm_map, 'neonate_mali_map.csv')
+
+#################################################
+
+### GAMBIA ### 
+
+setwd('/Users/xing/Documents/gambia/bn_hdss')
+
+#read in files
+c2016 <- read_csv('VA_Child_WHO2016.csv')
+c2012 <- read_csv('VA_ChildForm_WHO2012.csv')
+n2016 <- read_csv('VA_Neonates_WHO2016.csv')
+n2012 <- read_csv('VA_Neonates_WHO2012.csv')
+
+# make mapping skeletons
+c2016_map <- vida::create_skeleton(c2016)
+c2012_map <- vida::create_skeleton(c2012)
+n2016_map <- vida::create_skeleton(n2016)
+n2012_map <- vida::create_skeleton(n2012)
+
+# save as csvs
+write.csv(c2016_map, 'c2016_map.csv')
+write.csv(c2012_map, 'c2012_map.csv')
+write.csv(n2016_map, 'n2016_map.csv')
+write.csv(n2012_map, 'n2012_map.csv')
+
+setwd('/Users/xing/Documents/gambia/bs_hdss')
+
+#read in files
+c08_12 <- read_csv('VA_Child_Indepth_2008-2012.csv')
+c12_16 <- read_csv('VA_Child_WHO2012-mid 2016.csv')
+c16 <- read_csv('VA_Child_WHO2016.csv')
+n08_12 <- read_csv('VA_Neonates_Indepth_2008-2012.csv')
+n12_16 <- read_csv('VA_Neonates_WHO2012-mid-2016.csv')
+n16 <- read_csv('VA_Neonates_WHO2016.csv')
+
+# make mapping skeletons
+c08_12_map <- vida::create_skeleton(c08_12)
+c12_16_map <- vida::create_skeleton(c12_16)
+c16_map <- vida::create_skeleton(c16)
+n08_12_map <- vida::create_skeleton(n08_12)
+n12_16_map <- vida::create_skeleton(n12_16)
+n16_map <- vida::create_skeleton(n16)
+
+# save as csvs
+write.csv(c08_12_map, 'c08_12_map.csv')
+write.csv(c12_16_map, 'c12_16_map.csv')
+write.csv(c16_map, 'c16_map.csv')
+write.csv(n08_12_map, 'n08_12_map.csv')
+write.csv(n12_16_map, 'n12_16_map.csv')
+write.csv(n16_map, 'n16_map.csv')
